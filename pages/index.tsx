10bdc24edda5a3789e0618b5c03aac912d9c85c7
Ali Arain <deistali2@gmail.com>
2023-01-22 22:34:21 +0500

    Changes to main file

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,22 +11,13 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Face Photo Restorer</title>
+        <title>Abhi Theek Kar ke deta hoon</title>
       </Head>
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-28 mt-20">
-        <a
-          href="https://twitter.com/nutlope/status/1615597965787435015"
-          target="_blank"
-          rel="noreferrer"
-          className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
-        >
-          Used by over <span className="font-semibold">55,000</span> happy
-          customers
-        </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
-          Restoring old photos{" "}
+          ATKKDH is AI restoring web app.{" "}
           <span className="relative whitespace-nowrap text-[#3290EE]">
             <SquigglyLines />
             <span className="relative">using AI</span>
